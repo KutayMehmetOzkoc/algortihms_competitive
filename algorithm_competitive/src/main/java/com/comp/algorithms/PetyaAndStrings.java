7148aa113ba764a9ccad80d0cf29a9dfc5bbe020
KutayMehmetOzkoc <kutayozkoc999@gmail.com>
2026-04-24 00:01:25 +0300

    PetyaAndStrings Algorithm

---
+++ algorithm_competitive/src/main/java/com/comp/algorithms/PetyaAndStrings.java
@@ -0,0 +1,14 @@
+package com.comp.algorithms;
+
+import java.util.Scanner;
+
+public class PetyaAndStrings {
+    public int PetyaAndStringsAnswer() {
+
+        Scanner scanner = new Scanner(System.in);
+        String firstWord = scanner.next().toLowerCase();
+        String secondWord = scanner.next().toLowerCase();
+        int result = 0;
+        return result = firstWord.compareTo(secondWord);
+    }
+}
